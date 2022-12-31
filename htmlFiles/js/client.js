8857c3ae47c8364aa39a6b1541a7dcb82b30f4ac
coloshword <aceliang2001@gmail.com>
2022-12-30 22:07:24 -0500

    buttons now reset

--- htmlFiles/js/client.js
+++ htmlFiles/js/client.js
@@ -7,11 +7,15 @@ createGameBtn.addEventListener("click", function(e) {
 
 function showPopup(option) {
     let popup;
+    let remove;
     if(option == 1) {
         popup = document.getElementById("createGamePopup");
+        remove = document.getElementById("joinGamePopup");
     }
     else{
         popup = document.getElementById("joinGamePopup");
+        remove = document.getElementById("createGamePopup");
     }
     popup.classList.add("open-popup");
+    remove.classList.remove("open-popup");
 }
